2a5664a661b63be97681c7622de67ca55051be7b
Hajnalka Oltyan <hajnalka.oltyan@gmail.com>
2023-10-12 22:23:28 +0200

    Create test cases for the fizzbuzz method

--- lib/spec/solver_spec.rb
+++ lib/spec/solver_spec.rb
@@ -16,3 +16,22 @@ describe Solver do
       expect(solver.reverse(' ')).to eq(' ')
     end
   end
+
+  describe '#fizzbuzz' do
+    it 'returns fizz for input 3' do
+      expect(solver.fizzbuzz(3)).to eq('fizz')
+    end
+
+    it 'returns buzz for input 5' do
+      expect(solver.fizzbuzz(5)).to eq('buzz')
+    end
+
+    it 'returns fizzbuzz for input 15' do
+      expect(solver.fizzbuzz(15)).to eq('fizzbuzz')
+    end
+
+    it 'returns the input as a string for input 7' do
+      expect(solver.fizzbuzz(7)).to eq('7')
+    end
+  end
+end
