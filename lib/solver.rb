6821ffd56c3ee653ac082435d493b84064668834
Rysth <johnpalacios.t@gmail.com>
2023-10-12 16:03:27 -0500

    Implement fizzbuzz method

--- lib/solver.rb
+++ lib/solver.rb
@@ -1,10 +1,28 @@
 class Solver
-  def factorial
+
+  # Arguments
+  # @n = Number to be calculated
+  def factorial(n)
     # Implement logic here
   end
   
+  # Arguments
   # @word = String to be reversed
   def reverse(word)
     word.reverse
+  end
+
+  # Arguments
+  # @n = Number to be calculated
+  def fizzbuzz(n)
+    if (n % 3).zero? && (n % 5).zero?
+      'fizzbuzz'
+    elsif (n % 3).zero?
+      'fizz'
+    elsif (n % 5).zero?
+      'buzz'
+    else
+      n.to_s
+    end
   end
 end
